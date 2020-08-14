7e22a0c2e4a79d47d28f69ce97baa246cd8f45f1
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-08-14 02:37:45 -0400

    created more components and styling

---
+++ client/src/Components/PortfolioCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./style.css";
+
+
+function PortfolioCard(props){
+    return(
+                <div className= "sibel-stock">
+                    <a href={props.imgLink} target="blank">        
+                    <img alt="icon"  className= "imgSz rounded mx-auto d-block"  alt="clip of app" 
+                    src={props.src} 
+                    />
+                    </a>
+                    <div className="sibel-photo-bar"> 
+                        <a className="stronger" href={props.href} rel="noopener noreferrer" target="_blank">
+                       {props.text}
+                        </a>
+                        <br/>
+                        <a href={props.hrefRepo} rel="noopener noreferrer" target="_blank">
+                            Repo Link
+                        </a>
+                    </div>
+                </div>
+
+    )
+}
+
+export default PortfolioCard 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,12 +11,51 @@ function Home(){
 
                 <div className="col">
 
+                    <img className="sibel-photo" alt="profile" src={profilePic}/>
+
                     <h3> About Me</h3>
+                <p>
+                   </p>
 
                 </div>
 
             </div>
-            
+            <Row>
+                <Column4>
+                    <SkillCard
+                    heading="Languages, Environment, Frameworks"
+                    bullet1="JavaScript"
+                    bullet2="HTML/CSS"
+                    bullet3="React"
+                    bullet4="Node"
+                    bullet5="Jquery"
+                    />
+                </Column4>
+
+                <Column4>
+                    <SkillCard
+                    heading="Databases"
+                    bullet1="Oracle"
+                    bullet2="MongoDB/Moongoose"
+                    bullet3="MySQL/Sequelize"
+                    />
+                </Column4>
+
+                <Column4>
+                    <SkillCard
+                      heading="Soft skills"
+                      bullet1="Management and Coaching"
+                      bullet2="Detail Oriented"
+                      bullet3="Adaptable"
+                      bullet4="Quick learner"
+                      />
+                </Column4>
+
+            </Row>
+            <br/>
+            <br/>
+            <br/>
+
         </Container>
       
     )
