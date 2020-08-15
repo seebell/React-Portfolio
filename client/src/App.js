b3e6e09ecd8adf85eeab8eec879031ff5299e9bb
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-08-15 13:25:22 -0400

    heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
-import Home from './Pages/Home';
+import Home from './pages/Home';
 import Nav from "./Components/Nav";
 import Footer from "./Components/Footer";
 import Wrapper from './Components/Wrapper';
-import Portfolio from "./Pages/Portfolio";
-import Contact from "./Pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
 
 
